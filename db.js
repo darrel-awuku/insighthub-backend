f3a58a7bba62c1bbb33d375326b43242d5e6e0d6
darrel-awuku <darrel.awuku@acity.edu.gh>
2026-04-30 02:30:12 +0000

    added render link

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  password: "viper7thegamer",
-  host: "localhost",
-  port: 5432,
-  database: "insighthub"
+  connectionString: "postgresql://insighthub_db_svcl_user:nOx8UbmuKzCljqWFvcq2YVxAfID4tpNv@dpg-d7pbgt7avr4c73em4m90-a/insighthub_db_svcl",
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
--- insight-hub frontend/script.js
+++ insight-hub frontend/script.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:5000";
+const API = "https://insighthub-backend-hiog.onrender.com";
 
 console.log("SCRIPT LOADED");
 
